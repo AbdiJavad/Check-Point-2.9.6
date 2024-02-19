c7bda98d794a4df9bdf5d7c2a890d9eafcadfa5e
AbdiJavad <Mirjavadabdi@gmail.com>
2024-02-19 23:00:27 +0330

    Updated,Changed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-    
+
     public static void main(String[] args) {
+        
         
         System.out.println("25 / 4 is " + 25 / 4);
         System.out.println("25 / 4.0 is " + 25 / 4.0);
